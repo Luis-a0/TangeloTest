176aed7fb20b4a5d51ced9ac45383d13c847d4d6
Luis Aviles <lumaaviles0@gmail.com>
2022-08-28 17:33:25 -0500

    Modularización de los procesos para la generacion del diccionario.

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
 import requests
 import pandas as pd
-import hashlib
-import time
+from procesing import dict_generate
+
+URL = "https://restcountries.com/v3.1/all"
 
 if __name__ == '__main__':
-    URL = "https://restcountries.com/v3.1/all"
     data = requests.get(URL)
-    dicc = {'region':[], 'city_name':[], 'language':[], 'time':[]}
 
     if data.status_code == 200:
-        data_json = data.json()
-
-        for i in data_json:
-            start = time.perf_counter()
-            dicc['city_name'].append(i['name']['common'])
-            dicc['region'].append(i['region'])
-            if('languages'in i):
-                dicc['language'].append(hashlib.sha1(list(i['languages'].values())[0].encode()).hexdigest())
-            else:
-                dicc['language'].append("")
-            end = time.perf_counter()
-            dicc['time'].append((end - start) * 1000)
-
+        dicc = dict_generate(data)
+    
     df = pd.DataFrame(dicc)
     df.to_json('data.json')
 
-    print(f"""Valor minimo: {df['time'].min()}, 
-valor promedio: {df['time'].mean()},
-valor maximo: {df['time'].max()},
-valor total: {df['time'].sum()}""")
+    v_min, v_mean, v_max, v_total = df['time'].min(), df['time'].mean(), df['time'].max(),df['time'].sum()
+
+    print(f"Valor minimo: {v_min}, valor promedio: {v_mean}, valor maximo: {v_max}, valor total: {v_total}")
